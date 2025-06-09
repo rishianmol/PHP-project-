aaea48350d3ed9678cb630537d5454477f91bb97
rishianmol <anmolrishi193@gmail.com>
2025-06-09 21:44:56 +0530

    Update connection.php

--- Online-voting-system-project-in-php-master/connection.php
+++ Online-voting-system-project-in-php-master/connection.php
@@ -1,3 +1,3 @@
 <?php 
-$con = mysqli_connect("localhost","root","","polltest") or die ("error" . mysqli_error($con));
+$con = mysqli_connect("ec2-13-126-202-218.ap-south-1.compute.amazonaws.com","admin","","accounts") or die ("error" . mysqli_error($con));
 ?>
